d10fe626f0cd2839aa603d078431eb3cc935e823
Javier Prieto <jprieton@gmail.com>
2015-08-20 17:13:59 -0430

    Votos Widget del admin

--- includes/class-poll.php
+++ includes/class-poll.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace jptt\poll;
+namespace jptt;
 
-defined('ABSPATH') or die('No direct script access allowed');
+defined( 'ABSPATH' ) or die( 'No direct script access allowed' );
 
 class Poll {
 
@@ -10,8 +10,80 @@ class Poll {
 
 	}
 
-	public function add_vote($poll_id, $vote_value) {
-		
+	private function add_vote( $poll_id, $vote_value ) {
+
+	}
+
+	public static function create_schema() {
+		global $wpdb;
+		$wpdb instanceof \wpdb;
+
+		$charset = !empty( $wpdb->charset ) ?
+						"DEFAULT CHARACTER SET {$wpdb->charset}" :
+						'';
+
+		$collate = !empty( $wpdb->collate ) ?
+						"COLLATE {$wpdb->collate}" :
+						'';
+
+		$query = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}polls` ("
+						. "`poll_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,"
+						. "`post_id` bigint(20) unsigned NOT NULL DEFAULT '0',"
+						. "`vote_ip` bigint(20) DEFAULT NULL,"
+						. "`vote_value` bigint(20),"
+						. "`vote_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+						. "PRIMARY KEY (`poll_id`),"
+						. "KEY `post_id` (`post_id`)"
+						. ") ENGINE=InnoDB {$charset} {$collate} AUTO_INCREMENT=1";
+		$wpdb->query( $query );
+	}
+
+	public function vote() {
+		global $wpdb;
+		$Input = new core\Input();
+
+		$post_id = (int) $Input->post( 'poll_id' );
+		if ( $post_id === 0 ) {
+			return FALSE;
+		}
+		$poll_options = (array) get_post_meta( $post_id, 'poll_options', TRUE );
+		$poll_keys = array_keys( $poll_options );
+
+		$vote_value = (int) $Input->post( 'poll_vote', FILTER_SANITIZE_NUMBER_INT );
+
+		if ( !in_array( $vote_value, $poll_keys ) || !is_numeric( $vote_value ) ) {
+			return FALSE;
+		}
+
+		if ( !empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+			$vote_ip = $_SERVER['HTTP_CLIENT_IP'];
+		} elseif ( !empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+			$vote_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} else {
+			$vote_ip = $_SERVER['REMOTE_ADDR'];
+		}
+
+		if ( !filter_var( $vote_ip, FILTER_VALIDATE_IP ) ) {
+			return FALSE;
+		}
+
+		$vote_ip = ( $vote_ip == '::1' ) ? ip2long( '127.0.0.1' ) : ip2long( $vote_ip );
+
+		$voted = $wpdb->get_var( "SELECT * FROM `{$wpdb->prefix}polls` WHERE post_id = '{$post_id}' AND vote_ip = '{$vote_ip}' LIMIT 1" );
+
+		if ( empty( $voted ) ) {
+			$wpdb->insert( "{$wpdb->prefix}polls", compact( 'post_id', 'vote_ip', 'vote_value' ) );
+		}
+	}
+
+	public static function get_total_votes( $post_id, $vote_value = NULL ) {
+		global $wpdb;
+		$query = "SELECT count(*) as total FROM `{$wpdb->prefix}polls` WHERE post_id = '{$post_id}'";
+
+		if ( is_numeric( $vote_value ) ) {
+			$query .= " AND vote_value = {$vote_value}";
+		}
+		return $wpdb->get_var( $query );
 	}
 
 }
--- module.php
+++ module.php
@@ -11,20 +11,47 @@
  * Author URI: https://github.com/jprieton/
  * License: GPL2
  */
-defined('ABSPATH') or die('No direct script access allowed');
+defined( 'ABSPATH' ) or die( 'No direct script access allowed' );
 
-define('JPTT_POLL_PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('JPTT_POLL_PLUGIN_URI', plugin_dir_url(__FILE__));
+define( 'JPTT_POLL_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'JPTT_POLL_PLUGIN_URI', plugin_dir_url( __FILE__ ) );
 
+include_once JPTT_POLL_PLUGIN_PATH . 'includes/class-poll.php';
 include_once JPTT_POLL_PLUGIN_PATH . 'includes/register-post-type.php';
 include_once JPTT_POLL_PLUGIN_PATH . 'includes/register-meta-boxes.php';
 
-register_activation_hook(__FILE__, function() {
-	include_once JPTT_POLL_PLUGIN_PATH . 'includes/class-schema.php';
-	jptt\poll\Schema::create_poll_schema();
-});
+register_activation_hook( __FILE__, function() {
+	jptt\Poll::create_schema();
+} );
 
-add_action('jptt_load_modules', function() {
-	var_dump(  __FILE__);
-});
+add_action( 'wp_ajax_user_poll_vote', function() {
+	$Poll = new jptt\Poll();
+	$Poll->vote();
+}, 10 );
+
+/**
+ *
+ * @param WP_Post $post
+ */
+function metabox_callback( $post = null ) {
+	$poll_options = (array) get_post_meta( $post->ID, 'poll_options', TRUE );
+
+	$total = jptt\Poll::get_total_votes( $post->ID );
+
+	$format = '<div style="padding:5px">%s<div style="border: 1px solid darkgray; background-color: lightgray; padding: 5px 0; box-sizing: border-box; width:%s"></div></div>';
+	foreach ( $poll_options as $key => $option ) {
+		$count = jptt\Poll::get_total_votes( $post->ID, $key );
+		$percent = ($count * 100) / 18;
+
+		printf( $format, "{$option} ({$count})", $percent . '%' );
+	}
+}
+
+function adding_custom_meta_boxes() {
+	add_meta_box(
+					'metabox-id', __( 'Votes', 'jptt' ), 'metabox_callback', 'poll', 'side', 'high'
+	);
+}
+
+add_action( 'add_meta_boxes', 'adding_custom_meta_boxes', 10, 2 );
 
